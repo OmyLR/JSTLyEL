dec24b77b95ab1666a515795e5c9e1038e23c55c
OmyLR <omysam@gmail.com>
2018-10-22 11:46:15 +0100

    Initial commit

---
+++ src/controladores/ServletInit.java
@@ -0,0 +1,55 @@
+package controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletInit
+ */
+@WebServlet("/ServletInit")
+public class ServletInit extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletInit() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		Object[] objects = new Object[5];
+		objects[0] = "cero";
+		objects[1] = 1;
+		objects[2] = 0.2;
+		objects[3] = 'T';
+		objects[4] = "cuatro";
+		getServletContext().setAttribute("vector", objects);
+		System.out.println("Iniciando Servlet");
+	}
+
+}
