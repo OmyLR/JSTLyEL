ceb928a988864fe0f27182867581bbbdb413095c
OmyLR <omysam@gmail.com>
2018-10-22 12:06:56 +0100

    uno
    uno

--- src/controladores/ServletInit.java
+++ src/controladores/ServletInit.java
@@ -27,7 +27,8 @@ public class ServletInit extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String tipo = (String) request.getParameter("tipo");
+		System.out.println(tipo);
 	}
 
 	/**
